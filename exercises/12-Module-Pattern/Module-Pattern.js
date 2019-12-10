7556a24d0afcab2073c12cb4531df8ef7327758b
Dan Lubbers <lubbers.dan@gmail.com>
2019-12-09 20:11:27 -0500

    working on module pattern exercise

--- exercises/12-Module-Pattern/Module-Pattern.js
+++ exercises/12-Module-Pattern/Module-Pattern.js
@@ -18,10 +18,53 @@
  *
  */
 
-const { studentGrades } = require("../12-Module-Pattern/students");
+const studentGrades = [
+  {
+    firstName: "Jane",
+    lastName: "Doe",
+    grades: [50, 60, 70, 20, 10, 20]
+  },
+  {
+    firstName: "John",
+    lastName: "Doe",
+    grades: [100, 100, 100, 100, 100, 90]
+  },
+  {
+    firstName: "Michael",
+    lastName: "Jackson",
+    grades: [50, 40, 20, 20, 30, 20]
+  },
+  {
+    firstName: "Bill",
+    lastName: "Clinton",
+    grades: [70, 60, 70, 80, 100, 70]
+  },
+  {
+    firstName: "Joe",
+    lastName: "Miller",
+    grades: [70, 60, 70, 80, 100, 70]
+  },
+  {
+    firstName: "Billy",
+    lastName: "Jean",
+    grades: [70, 90, 90, 80, 100, 80]
+  }
+];
+
+// const { studentGrades } = require("../12-Module-Pattern/students");
 
 const TeacherGradeBook = (() => {
-  return {};
+  
+  const average = studentGrades.reduce((acc, curr) => acc.grades + curr.grades, 0) ;
+  console.log(average);
+  
+  
+
+  return {
+    // getFailingStudents:() => {
+    //   averages.filter(())
+    // }
+  };
 })();
 
 module.exports = {
